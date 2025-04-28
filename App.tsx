3b71933b0c6623d6188b0b847f4af036f78f354f
CraigAdumuah <Craigadumuah123@gmail.com>
2025-04-28 10:57:51 +0100

    mc

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 // App.tsx
+
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import signin from "./Screens/signin";
+
+import SignIn from "./Screens/Signin";
 import Register from "./Screens/Register";
 import Dashboard from "./Screens/Dashboard";
 import Settings from "./Screens/Settings";
@@ -12,8 +14,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signin">
-        <Stack.Screen name="signin" component={signin} options={{ headerShown: false }} />
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignIn} options={{ headerShown: false }} />
         <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
         <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
         <Stack.Screen name="Settings" component={Settings} options={{ headerShown: false }} />
